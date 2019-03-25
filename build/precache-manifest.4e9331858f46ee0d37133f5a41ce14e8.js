bbced3b4a27b0fc90b3bf3b36a49466df8cbd7ab
李望喜 <1216239264@qq.com>
2019-03-25 10:08:43 +0800

    Add files via upload

---
+++ build/precache-manifest.4e9331858f46ee0d37133f5a41ce14e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a3a92b2b037bbf75f30",
+    "url": "/static/css/main.2925d063.chunk.css"
+  },
+  {
+    "revision": "8a3a92b2b037bbf75f30",
+    "url": "/static/js/main.5b5578d2.chunk.js"
+  },
+  {
+    "revision": "deddee7656052f8a557e",
+    "url": "/static/js/runtime~main.b9ad1ddc.js"
+  },
+  {
+    "revision": "7d68957b34619048c505",
+    "url": "/static/js/2.3db50b5f.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c76ff341e7ede3a3618725b9ed1ca409",
+    "url": "/index.html"
+  }
+];
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Button } from 'antd-mobile';
+import './App.less';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <span>234234</span>
+        <Button>Buttom</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
